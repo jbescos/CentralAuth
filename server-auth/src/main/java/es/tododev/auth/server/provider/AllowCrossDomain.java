0bcf0b1887d43a7acd41910a9a19d0b516b6aa55
jbescos <jorgebescos@gmail.com>
2016-11-27 11:16:08 +0100

    Allow cross origin POST, DELETE, PUT

--- server-auth/src/main/java/es/tododev/auth/server/provider/AllowCrossDomain.java
+++ server-auth/src/main/java/es/tododev/auth/server/provider/AllowCrossDomain.java
@@ -15,7 +15,7 @@ public class AllowCrossDomain implements ContainerResponseFilter {
 		response.getHeaders().add("Access-Control-Allow-Origin", "*");
 		response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
 		response.getHeaders().add("Access-Control-Allow-Credentials", "true");
-		response.getHeaders().add("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD");
+		response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
 	}
 
 }
